5bfc1885f98d0900835b2a514c0745321cee3724
iamrhn <rhn7114@gmail.com>
2025-04-17 21:43:23 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,40 @@
+/*Data Types: Primitive & Non-Primitive
+
+Primitive
+1. Number - 123
+2. String - "This is a string"
+3. Boolean - True or False
+4. Symbol - A unique and immutable primitive used as obj.
+5. Undefined - Undefined value.
+6. Null - No value or empty.
+7. BigINT - Used to represent integers larger than numbers.
+
+Non-Primitive
+1.Object
+Arrays - An ordered list of values, indexed by numbers.
+Functions - A block of codes designed to perform a particular task, which is also a type of object.*/
+
+let fullName = "Rehan"; //String
+let rollNo = 7; //Number
+let temprature = null; //null(Object type)
+let loggedIn = false; //boolean
+
+console.log(fullName);
+console.log(rollNo);
+console.log(temprature);
+
+//How to check datatype?
+console.log(typeof rollNo);
+console.log(typeof 7);
+console.log(typeof fullName);
+console.log(typeof "Rehan");
+console.log(typeof temprature);
+console.log(typeof loggedIn);
+console.log(typeof undefined);
+
+
+
+
+
+
+
