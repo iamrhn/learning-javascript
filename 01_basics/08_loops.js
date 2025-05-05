f4cf8336959187e0a3529d107845b1435cde53ad
Rehan <rhn7114@gmail.com>
2025-05-06 01:56:17 +0530

    loops

---
+++ 01_basics/08_loops.js
@@ -0,0 +1,11 @@
+// Loops - We use loops to perform repeated actions.
+//For Loops, For In Loops, For of Loops, While Loops, Do While Loops
+
+/*For Loops - Syntax
+for (let i = 0; i < 5; i++) {
+    console.log(i); // 0 to 4
+  }*/
+
+    for(let i = 0; i < 10; i++){
+        console.log(i+1)
+    }
