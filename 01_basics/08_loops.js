673e76b3be41002c1cace316700dc73e87688a38
Rehan <rhn7114@gmail.com>
2025-05-12 06:17:04 +0530

    loops

--- 01_basics/08_loops.js
+++ 01_basics/08_loops.js
@@ -1,53 +1,97 @@
-// Loops - We use loops to perform repeated actions.
-//For Loops, For In Loops, For of Loops, While Loops, Do While Loops
+/*Loops are used to perform repeated actions.
 
-/*For Loops - Syntax
+/1. For Loop
+/Syntax:
+for (initialization; condition; increment) {
+  // code to be executed on each iteration
+}
+
+Syntax:
 for (let i = 0; i < 5; i++) {
-    console.log(i); // 0 to 4
-  }*/
-
-    for (i = 0; i < 10; i++){
-        console.log(i+1)
-    }
-
-    //Reverse
-    for (number = 100; number > 0; number--){
-        console.log(number)
-    }
-    
-let factorial = 1;
-let n = 4
-factorial = (Number(factorial))
-for (i = 1; i <= n; i++) {
-    factorial *= i;
-}
-console.log('Factorial of ' + n + " is " + factorial)
-
-//For In Loops
-/*Syntax
-for (let key in object) {
-    // code to be executed
-}*/
-//For In loops also works with arrays.
-
-const obj = {
-    John: 100,
-    Alan: 80,
-    Mark: 60,
-    Anthony: 40,
-    Steven: 20,
-}
-for(let a in obj){
-    console.log('Marks of ' + a + ' are ' + obj[a])
-}
-
-//For Of Loops
-/*Syntax
-for (let value of iterable) {
-    // code to be executed
+  console.log(i);
 } */
 
- for (let a of "Rehan"){
-     console.log(a)
+  for (let i = 0; i < 5; i++){
+    console.log(i+1);
+  }
+
+  /*Questions:
+  Print numbers from 1 to 10*/
+
+for (let number = 0; number < 10; number++){
+    console.log(number+1);
 }
+
+//Print even numbers from 1 to 20.
+
+for (let evenNumber = 0; evenNumber < 20; evenNumber++){
+    if(evenNumber % 2 === 0)
+    console.log(evenNumber)
+}
+
+//Print odd numbers from 1 to 20.
+
+for (let oddNumber = 1; oddNumber < 20; oddNumber++){
+    if(oddNumber % 2 === 1)
+        console.log(oddNumber)
+}
+
+//Print numbers from 10 to 1 in reverse.
+
+for (let reverseNumber = 10; reverseNumber > 0; reverseNumber--){
+    console.log(reverseNumber)
+}
+
+//Find the sum of numbers from 1 to 100.
+
+let sum = 0;
+for (let num = 1; num <= 100; num++){
+    sum += num
+}
+console.log(sum)
+
+//Find the natural number of n.
+
+let sum1 = 0;
+let n = 4;
+for (let i = 0; i < n; i++){
+    sum1 += i
+}
+console.log(n + sum1);
+
+/*2. For In Loop
+Syntax: for (let key in object) {
+    // code to execute
+} 
+Note:- For In loop can be used to access key, value or key values.    
+*/
+
+const obj = {
+    Rehan : 100,
+    Steven : 80,
+    John : 60,
+    Mick : 40,
+    Nick : 20,
+    Lambrard : 0,
+}
+
+for (let a in obj){
+    console.log("Marks of " + a + " are " + obj[a]); 
+}
+
+/*3. For Of Loop
+Syntax: for (let value of iterable) {
+    // code to execute
+} 
+Note:- Should be iterable and mostly used for arrays. */
+
+for (let c of "Rehan Shaikh"){
+    console.log(c)
+}
+
+/*4. While Loop
+while (condition) {
+  // code block to be executed if the condition is true and the loop will continue to run until condition becomes false
+} */
+
 
